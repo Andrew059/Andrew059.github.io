d33f1268d3f5709ced26770ed34c859147e05b73
Andrew059 <71473456+Andrew059@users.noreply.github.com>
2021-06-08 15:03:19 -0400

    Update EndScene.js

--- EndScene.js
+++ EndScene.js
@@ -5,22 +5,30 @@ class EndScene extends Phaser.Scene {
 
   preload() {
     this.load.image('bobaend', 'bobaend.png');
+    this.load.audio('nooo', 'noooo.mp3'); 
+
   }
 
 
   create() {
+    let soundSample3 = this.sound.add('nooo');
+      soundSample3.play();
     this.add.image(210, 280,'bobaend').setScale(1.2);
-    this.add.text(150, 250, 'Game Over', { fontSize: '20px', fill: '#000000' });
-    this.add.text(112, 270, 'Click to Restart', { fontSize: '20px', fill: '#000000' });
-    
-		
+    this.add.text(140, 140, 'Game Over', { fontSize: '50px', fill: '#000000', fontFamily: 'Odibee Sans' });
+    this.add.text(112, 180, 'Click to Restart', { fontSize: '50px', fill: '#000000', fontFamily: 'Odibee Sans'  });
+    gameState.scoreText = this.add.text(60, 490, ' Your Final Score is 0', { fontSize: '50px', fill: '#808080', fontFamily: 'Odibee Sans'});
+    gameState.scoreText.setText(`Your Final Score is ${gameState.score}`);
+
     this.input.on('pointerup', () =>{
       gameState.score = 0;
     	this.scene.stop('EndScene');
 	    this.scene.start('StartScene');
+      
     });
     }
   }
+
+
 
 
 
