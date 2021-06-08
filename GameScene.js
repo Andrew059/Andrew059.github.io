635583936316d89809590c5f8e015b912e07422c
Andrew059 <71473456+Andrew059@users.noreply.github.com>
2021-06-08 13:58:55 -0400

    Update GameScene.js

--- GameScene.js
+++ GameScene.js
@@ -4,17 +4,23 @@ class GameScene extends Phaser.Scene {
   }
   
   preload() {
-     
+    this.load.audio('jetpack', 'jetpack.mp3'); 
     this.load.image('boba', 'copy_321958841.png');
     this.load.image('stormtrooper', 'stormtrooper.png');
     this.load.image('scouttrooper', 'scouttrooper.png');
     this.load.image('platforms', 'terrain.png');
     this.load.image('platforms2', 'terrain2.png');
-  }
+    this.load.image('fireball', 'fireball.png');
+    this.load.image('laser', 'laser.png');
+    this.load.image('deathstar', 'deathstar.png');
 
+  }
+ 
   create() {
     
-    gameState.player = this.physics.add.sprite(50, 500, 'boba').setScale(.6);
+     this.add.image(200,300 , 'deathstar').setScale(1.6);
+
+     gameState.player = this.physics.add.sprite(80, 500, 'boba').setScale(.5);
       
     const platforms = this.physics.add.staticGroup();
     
@@ -28,50 +34,146 @@ class GameScene extends Phaser.Scene {
     platforms.create(140, 590, 'platforms').setScale(2, .6).refreshBody();
     platforms.create(120, 590, 'platforms').setScale(2, .6).refreshBody();
     platforms.create(60, 590, 'platforms').setScale(2, .6).refreshBody();
-    platforms.create(20, 590, 'platforms').setScale(2, .6).refreshBody();
-    
-    gameState.player.setCollideWorldBounds(true);
+   platforms.create(20, 590, 'platforms').setScale(2, .6).refreshBody();
+
+    const fireball = this.physics.add.staticGroup();
+
+     fireball.create(10, 10, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 40, 'fireball').setScale(0.1, .1).refreshBody();
+
+    fireball.create(10, 70, 'fireball').setScale(0.1, .1).refreshBody();
+
+    fireball.create( 10, 100, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 130, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 160, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 190, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 220, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 250, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 280, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 310, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 340, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 370, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 400, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 430, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 460, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 490, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 520, 'fireball').setScale(0.1, .1).refreshBody();
+    fireball.create(10, 550, 'fireball').setScale(0.1, .1).refreshBody();
+   this.physics.add.collider(gameState.player, fireball, () => {
+   setPlatformsLoop.destroy();
+	 this.scene.stop('GameScene');
+	 this.scene.start('EndScene');
+  });
+   
+     gameState.player.setCollideWorldBounds(true);
 
     this.physics.add.collider(gameState.player, platforms);
 
     gameState.cursors = this.input.keyboard.createCursorKeys();
 
-    
-    const troopers = this.physics.add.group();
+    gameState.enemy = this.physics.add.sprite(400, 510, 'stormtrooper').setScale(.8);
 
-    const troopersList = ['stormtrooper', 'scouttrooper'];
-
-function setPlatforms() {
-  for (let i = 0; i < 10; i++) {
-    let platPositions = [];
-     let randomPlatformsX = Math.random() * 420 + 1;
-     let randomPlatformsY = Math.random() * 590 - 30;
-     platPositions.push({ x: randomPlatformsX, y: randomPlatformsY });
-     platPositions.forEach(plat => {
-      platforms.create(plat.x, plat.y, 'platforms2');
-     });
-  
-     }
-}
-setPlatforms();
+    gameState.enemy.setCollideWorldBounds(true);
    
+    
+   this.physics.add.collider(gameState.enemy, platforms);
+
+   gameState.enemy.move = this.tweens.add({
+   targets: gameState.enemy,
+      x: 60,
+      ease: 'Linear',
+      duration: 5000,
+      repeat: -1,
+      yoyo: true
+  });
+   function setShoot() {
+     const laser = this.physics.add.sprite(gameState.enemy.x - 40, gameState.enemy.y, 'laser').setScale(.5);
+     laser.body.allowGravity = false;
+     laser.body.immovable = true;
+    
+    const destx = gameState.player.x;
+    const desty = gameState.player.y;
+    const speed = 100;
+    const startx = gameState.enemy.x;
+    const starty = gameState.enemy.y;
+
+    const dx = destx - startx;
+    const dy = desty - starty;
+    const totalDistance = Math.sqrt(dx * dx + dy + dy);
+    const percent = speed / totalDistance;
+    laser.body.velocity.x = percent * dx;
+    laser.body.velocity.y = percent * dy;
+
+   }
+
+  
+    
+
+
+   const setLaserLoop = this.time.addEvent({
+    delay: 1000,
+    callback: setShoot,
+    callbackScope: this,
+    loop: true,
+ });
+         gameState.scoreText = this.add.text(200, 10, 'Score: 0', { fontSize: '15px', fill: '#808080' });
+
+
+ function setPlatforms () {
+
+    const yCoord = Math.random() * 580 - 130;
+    const platforms2 = this.physics.add.sprite(500, yCoord, 'platforms2');
+    platforms2.body.allowGravity = false;
+    platforms2.body.immovable = true;
+    platforms2.body.velocity.x = -100;
+   
+  this.physics.add.collider(gameState.player, platforms2, () => {
+   setPlatformsLoop.destroy();
+	 this.scene.stop('GameScene');
+	 this.scene.start('EndScene');
+  });
+  this.physics.add.collider(fireball, platforms2, () => {
+   platforms2.destroy();
+   //need to add scoring method
+   gameState.score += 10;
+    gameState.scoreText.setText(`Score: ${gameState.score}`);
+  });
+}
+ //function setStormtrooper () {
+    //const yCoord = Math.random() * 580;
+    //platforms2.create(420, 550, 'stormtrooper');
+ //}
+
+  const setPlatformsLoop = this.time.addEvent({
+    delay: 500,
+    callback: setPlatforms,
+    callbackScope: this,
+    loop: true,
+ });
+  
+  
+
+  
+
+ this.physics.add.collider(gameState.player, gameState.enemy, () => {
+   setPlatformsLoop.destroy();
+	 this.scene.stop('GameScene');
+	 this.scene.start('EndScene');
+  });
+
 }
 
-  update() {
+update() {
+    let soundSample = this.sound.add('jetpack');
     if (gameState.cursors.left.isDown) {
-			gameState.player.setVelocityX(-160);
+			gameState.player.setVelocityX(-300);
 		} else if (gameState.cursors.right.isDown) {
-			gameState.player.setVelocityX(160);
+			gameState.player.setVelocityX(300);
 		} else if (gameState.cursors.up.isDown){
-      gameState.player.setVelocityY(-160)
+      gameState.player.setVelocityY(-300);
+      soundSample.play();
     } else {
 			gameState.player.setVelocityX(0);
 		}
-  }
-
-  
- 
-
-
-  
+  } 
 }
