21a59632bc5d5c523096e50787a1d431405f7ae5
Andrew059 <71473456+Andrew059@users.noreply.github.com>
2021-06-09 14:30:30 -0400

    Update StartScene.js

--- StartScene.js
+++ StartScene.js
@@ -10,15 +10,15 @@ class StartScene extends Phaser.Scene {
   }
 
   create() {
-    let soundSample4 = this.sound.add('yoda').setVolume(2);
-    soundSample4.play();
+    let soundSample8 = this.sound.add('yoda').setVolume(2);
+    soundSample8.play();
     this.add.image(200,300 , 'background');
     this.add.text(60,40, "Boba's Epic Adventure" , { fill: '#A9A9A9', fontSize: '50px', fontFamily: 'Odibee Sans'});
     this.add.text(90, 510, 'Ready for a Thrilling Experience?\n               Click to start!', { fill: '#A9A9A9', fontSize: ' 30px',  fontFamily: 'Odibee Sans'});
 			
     this.input.on('pointerup', () => {
-      let soundSample2 = this.sound.add('bobastart');
-      soundSample2.play();
+      let soundSample9 = this.sound.add('bobastart');
+      soundSample9.play();
       this.scene.stop('StartScene');
       this.scene.start('GameScene');
     });
