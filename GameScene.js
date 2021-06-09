0a6f60a00a20a64c4cc309eff9908ae6909ab430
Andrew059 <71473456+Andrew059@users.noreply.github.com>
2021-06-08 21:17:13 -0400

    Update GameScene.js

--- GameScene.js
+++ GameScene.js
@@ -243,7 +243,7 @@ class GameScene extends Phaser.Scene {
  //}
 
   const setPlatformsLoop = this.time.addEvent({
-    delay: 900,
+    delay: 1500,
     callback: setPlatforms,
     callbackScope: this,
     loop: true,
