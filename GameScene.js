fa895fc4fabfe097d9942eb09bd073c29ef518c3
Andrew059 <71473456+Andrew059@users.noreply.github.com>
2021-06-08 20:22:30 -0400

    Update GameScene.js

--- GameScene.js
+++ GameScene.js
@@ -291,14 +291,14 @@ class GameScene extends Phaser.Scene {
 update() {
 	let soundSample6 = this.sound.add('jetpack').setVolume(0.1);
     if (gameState.cursors.left.isDown) {
-			gameState.player.setVelocityX(-300);
+			gameState.player.setVelocityX(-500);
 		} else if (gameState.cursors.right.isDown) {
-			gameState.player.setVelocityX(300);
+			gameState.player.setVelocityX(500);
 		} else if (gameState.cursors.up.isDown){
 			         soundSample6.play();
-      gameState.player.setVelocityY(-300);
+      gameState.player.setVelocityY(-500);
     } else if  (gameState.cursors.down.isDown){
-    gameState.player.setVelocityY(300);
+    gameState.player.setVelocityY(500);
     } else {
 			gameState.player.setVelocityX(0);
 		}
