93ea64b976a59a857c0ff8cf1116add719c2211c
Andrew059 <71473456+Andrew059@users.noreply.github.com>
2021-06-09 14:29:54 -0400

    Update EndScene.js

--- EndScene.js
+++ EndScene.js
@@ -10,8 +10,8 @@ class EndScene extends Phaser.Scene {
 
 
   create() {
-    let soundSample8 = this.sound.add('nooo').setVolume(0.4);
-    soundSample8.play();
+    let soundSample7 = this.sound.add('nooo').setVolume(0.4);
+    soundSample7.play();
     this.add.image(210, 280,'bobaend').setScale(1.2);
     this.add.text(140, 140, 'Game Over', { fontSize: '50px', fill: '#000000', fontFamily: 'Odibee Sans' });
     this.add.text(32, 180, 'Refresh the Page to Restart', { fontSize: '50px', fill: '#000000', fontFamily: 'Odibee Sans'  });
